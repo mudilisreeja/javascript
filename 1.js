820ae721f290e1029a64ecd2af39626ff4afd77e
sreeja <sreeja.mudili@gmail.com>
2024-10-28 12:47:05 +0530

    variables and objects

---
+++ 1.js
@@ -0,0 +1,18 @@
+//creating a object
+const product = {
+title:"ballPen",
+price: 270,
+rating: 4,
+discount: 5,
+};
+console.log(product);
+//creating a const object profile
+const profile ={
+    name:"shradda",
+    posts:230,
+    followers:5320,
+    isfollows:true,
+
+};
+console.log(profile);
+console.log(typeof profile["followers"]);
