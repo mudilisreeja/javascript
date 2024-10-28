59b1907bcbcf9a6d96eb999e6094f41b06f73881
sreeja <sreeja.mudili@gmail.com>
2024-10-28 14:33:04 +0530

    html for 2.js

---
+++ 2.js
@@ -0,0 +1,11 @@
+let num = prompt("Enter number: ");
+if(num % 5 === 0 ) {
+    console.log("number is multiplied by 5");
+}
+    else {
+        console.log("number is not multiplied by 5");
+
+}
+
+
+ 
