837b3384d8e3118350764c096c623659c4bca7d9
sreeja <sreeja.mudili@gmail.com>
2024-10-29 10:58:23 +0530

    strings

---
+++ 5.js
@@ -0,0 +1,12 @@
+let fullname =prompt("Enter your fullname");
+console.log(fullname);
+let result="@"+fullname+fullname.length;
+console.log(result)
+/////////string methods/////////////
+let str="I am learning Javascript"
+console.log(str.length);
+console.log(str.slice(5,14));
+let str1 =str.replace("s","j");
+console.log(str1);
+let str2=str.charAt(6);
+console.log(str2);
