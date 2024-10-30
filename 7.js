883ab089edcf8c5c2a63d869a893e228a104da86
sreeja <sreeja.mudili@gmail.com>
2024-10-30 16:07:27 +0530

    functions

---
+++ 7.js
@@ -0,0 +1,23 @@
+function countvowels(str){
+let count=0;
+    for(const char of str){
+        console.log(char);
+        if(char === "a"||char ==="e"||char ==="i"||char ==="o"||char ==="u"){
+            count++;
+        }
+    }
+    console.log(count); 
+}
+   
+//arrow function
+ const countvow=(str)=>{
+    count=0;
+    for(const char of str){
+        console.log(char);
+        if(char === "a"||char ==="e"||char ==="i"||char ==="o"||char ==="u"){
+            count++;
+        }
+    }
+    console.log(count); 
+}  
+
