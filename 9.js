4bedb2b33e4bc66b73f22bd8051f23034e6d9e6d
sreeja <sreeja.mudili@gmail.com>
2024-11-01 11:10:44 +0530

    attributes

---
+++ 9.js
@@ -0,0 +1,12 @@
+let div = document.querySelector("div");
+console.log(div);
+console.log(div.getAttribute("id"));
+//get attribute
+let para = document.querySelector("p");
+let pa= para.setAttribute("class","newclass");
+console.log(pa);
+//node.style
+div.style.backgroundColor="green";
+div.style.color="white";
+div.style.fontSize="25px";
+
